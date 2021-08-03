58b6af28fe38f530c7d5e097827ed85f77999fbf
182961 <87044961+18296@users.noreply.github.com>
2021-08-03 22:34:20 +0000

    comments!

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r
 def cypher():
     while True:
         try: 
-            cypherkey = int(input("enter original cypher key\t"))
+            cypherkey = int(input("enter original cypher key\t")) 
             break
     
         except:
             pass
 
     while True:
-        yesno = input("do you want to cypher or decypher\t")
+        yesno = input("do you want to cypher or decypher\t") #convert cypher value to negative
         yesno = yesno.lower()
         if yesno == "decypher":
             cypherkey = -cypherkey
@@ -22,34 +22,33 @@ def cypher():
         else:
             pass
     codein = input("enter word\t")
-    codein = codein.lower()
+    codein = codein.lower() #prevents crashing from upper case
     codeinlist = list(codein)
     x=0
     wordlen = len(codein)
-    new_word = ''
+    new_word = '' #creates platform for new word
     
-    while 0<wordlen:
+    while 0<wordlen: #measures how many times it should repeat
         char=codeinlist[x]
         if char in alphabet:
-            index = alphabet.index(char)
-            #print(index)
+            index = alphabet.index(char)#finds letter place in alphabet
             x = x+1
             wordlen = wordlen-1
             while True:
                 if (index+cypherkey)>25:
-                    index = ((index+cypherkey)-(26+cypherkey))
+                    index = ((index+cypherkey)-(26+cypherkey))#changes number by cypher key
                 else:
                     break
                 
             new_letternum = (index+cypherkey)
             new_letter = (alphabet[new_letternum]) 
-            new_word = (new_word+new_letter)
+            new_word = (new_word+new_letter)#adds letter to new word
 
         else:
             wordlen = wordlen-1
             char=codeinlist[x]            
             new_word = (new_word+char)
-            x = x+1                        
+            x = x+1                        #ignores spaces and non alphabetical characters
     print(new_word)
 
 cypher()
