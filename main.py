bc840fa7e799a2ac346bd7c5d1485501c2109e82
182961 <87044961+18296@users.noreply.github.com>
2021-08-03 22:21:49 +0000

    fixing errors final code

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+
+
 def cypher():
     while True:
         try: 
@@ -30,6 +32,7 @@ def cypher():
         char=codeinlist[x]
         if char in alphabet:
             index = alphabet.index(char)
+            #print(index)
             x = x+1
             wordlen = wordlen-1
             while True:
