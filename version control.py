87a06af887a5dd19e451603ea426b715f4b41db7
182961 <87044961+18296@users.noreply.github.com>
2021-08-03 22:09:24 +0000

    can cypher string of characters

--- version control.py
+++ version control.py
@@ -1,7 +1,5 @@
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-
-
 def cypher():
     while True:
         try: 
@@ -22,20 +20,28 @@ def cypher():
         else:
             pass
     codein = input("enter word\t")
+
     codeinlist = list(codein)
     x=0
-    char=codeinlist[x]
-    if char in alphabet:
-            index = alphabet.index(char)
-            #print(index)
-            while True:
-                if (index+cypherkey)>25:
-                    index = ((index+cypherkey)-(26+cypherkey))
-                else:
-                    break
+    wordlen = len(codein)
+    new_word = ''
+    
+    while 0<wordlen:
+        char=codeinlist[x]
+        index = alphabet.index(char)
+          
+        x = x+1
+        wordlen = wordlen-1
+        while True:
+            if (index+cypherkey)>25:
+              index = ((index+cypherkey)-(26+cypherkey))
+            else:
+              break
                 
-    new_letternum = (index+cypherkey)
-    new_letter = (alphabet[new_letternum])
-    print(new_letter)
+        new_letternum = (index+cypherkey)
+        new_letter = (alphabet[new_letternum]) 
+        new_word = (new_word+new_letter)
+
+    print(new_word)
 
 cypher()
