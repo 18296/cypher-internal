8eda47557e29d04438c14c65b8c013df225cdb89
182961 <87044961+18296@users.noreply.github.com>
2021-08-05 00:55:27 +0000

    PEP8 checker

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 def cypher():
     while True:
-        try: 
-            cypherkey = int(input("enter original cypher key\t")) 
+        try:
+            cypherkey = int(input("enter original cypher key\t"))
             break
-    
+
         except:
             pass
 
     while True:
-        yesno = input("do you want to cypher or decypher\t") #convert cypher value to negative
+        yesno = input("do you want to cypher or decypher\t")  # convert cypher value to negative
         yesno = yesno.lower()
         if yesno == "decypher":
             cypherkey = -cypherkey
@@ -22,33 +22,32 @@ def cypher():
         else:
             pass
     codein = input("enter word\t")
-    codein = codein.lower() #prevents crashing from upper case
+    codein = codein.lower()  # prevents crashing from upper case
     codeinlist = list(codein)
-    x=0
+    x = 0
     wordlen = len(codein)
-    new_word = '' #creates platform for new word
-    
-    while 0<wordlen: #measures how many times it should repeat
-        char=codeinlist[x]
+    new_word = ''  # creates platform for new word
+    while 0 < wordlen:  # measures how many times it should repeat
+        char = codeinlist[x]
         if char in alphabet:
-            index = alphabet.index(char)#finds letter place in alphabet
+            index = alphabet.index(char)  # finds letter place in alphabet
             x = x+1
             wordlen = wordlen-1
             while True:
-                if (index+cypherkey)>25:
-                    index = ((index+cypherkey)-(26+cypherkey))#changes number by cypher key
+                if (index+cypherkey) > 25:
+                    index = ((index+cypherkey)-(26+cypherkey))  # changes number by cypher key
                 else:
                     break
-                
+
             new_letternum = (index+cypherkey)
-            new_letter = (alphabet[new_letternum]) 
-            new_word = (new_word+new_letter)#adds letter to new word
+            new_letter = (alphabet[new_letternum])
+            new_word = (new_word+new_letter)  # adds letter to new word
 
         else:
             wordlen = wordlen-1
-            char=codeinlist[x]            
+            char = codeinlist[x]
             new_word = (new_word+char)
-            x = x+1                        #ignores spaces and non alphabetical characters
+            x = x+1  # ignores spaces and non alphabetical characters
     print(new_word)
 
 cypher()
